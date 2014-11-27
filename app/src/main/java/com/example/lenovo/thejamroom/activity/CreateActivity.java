ec2909e2f893a754c23d9f8771be51798a63572f
avaneeshd <avaneeshdesai@gmail.com>
2014-11-26 22:24:47 -0700

    audio record changes

--- app/src/main/java/com/example/lenovo/thejamroom/activity/CreateActivity.java
+++ app/src/main/java/com/example/lenovo/thejamroom/activity/CreateActivity.java
@@ -42,24 +42,26 @@ public class CreateActivity extends Fragment {
     private static final String LOG_TAG = "AudioRecordTest";
     private static String mFileName = null;
 
-    private RecordButton mRecordButton = null;
     private MediaRecorder mRecorder = null;
 
-    private PlayButton   mPlayButton = null;
     private MediaPlayer mPlayer = null;
-
+    boolean isRecording = false;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater,container,savedInstanceState);
         final View view = inflater.inflate(R.layout.activity_create, container, false);
 
-        uploadFile();
+       // uploadFile();
 
         Button createBtnRecord = (Button)view.findViewById(R.id.createBtnRecord);
         createBtnRecord.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                onRecord(true);
+                if(isRecording == false)
+                    isRecording = true;
+                else
+                    isRecording = false;
+                onRecord();
             }
         });
         return view;
@@ -95,8 +97,10 @@ public class CreateActivity extends Fragment {
 
 
 
-    private void onRecord(boolean start) {
-        if (start) {
+    private void onRecord() {
+        if (isRecording) {
+            mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
+            mFileName += "/audiorecordtest.3gp";
             startRecording();
         } else {
             stopRecording();
@@ -141,6 +145,7 @@ public class CreateActivity extends Fragment {
         }
 
         mRecorder.start();
+
     }
 
     private void stopRecording() {
@@ -148,51 +153,4 @@ public class CreateActivity extends Fragment {
         mRecorder.release();
         mRecorder = null;
     }
-
-    class RecordButton extends Button {
-        boolean mStartRecording = true;
-
-        OnClickListener clicker = new OnClickListener() {
-            public void onClick(View v) {
-                onRecord(mStartRecording);
-                if (mStartRecording) {
-                    setText("Stop recording");
-                } else {
-                    setText("Start recording");
-                }
-                mStartRecording = !mStartRecording;
-            }
-        };
-
-        public RecordButton(Context ctx) {
-            super(ctx);
-            setText("Start recording");
-            setOnClickListener(clicker);
-        }
-    }
-
-    class PlayButton extends Button {
-        boolean mStartPlaying = true;
-
-        OnClickListener clicker = new OnClickListener() {
-            public void onClick(View v) {
-                onPlay(mStartPlaying);
-                if (mStartPlaying) {
-                    setText("Stop playing");
-                } else {
-                    setText("Start playing");
-                }
-                mStartPlaying = !mStartPlaying;
-            }
-        };
-
-        public PlayButton(Context ctx) {
-            super(ctx);
-            setText("Start playing");
-            setOnClickListener(clicker);
-        }
-    }
-
-
-
 }
